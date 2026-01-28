ff455f3bc72a2b54a5e9efe659e44c70a4d1fdf9
szodi <sandor.szodoray@codecool.com>
2026-01-28 19:05:48 +0100

    relocating subtree positioning

---
+++ src/app/components/my-node-tree/my-node-tree.ts
@@ -0,0 +1,160 @@
+import {Component, ComponentRef, computed, HostListener, inject, OnInit, signal, ViewContainerRef} from '@angular/core';
+import {TreeNodeStore} from '../../app.store';
+import {MyNode} from '../my-node/my-node';
+import {TreeNodeDto} from '@ptc-api-models/treeNodeDto';
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+@Component({
+  selector: 'app-my-node-tree',
+  imports: [],
+  templateUrl: './my-node-tree.html',
+  styleUrl: './my-node-tree.scss',
+})
+export class MyNodeTree implements OnInit {
+  treeNodeStore = inject(TreeNodeStore);
+
+  rootNode = this.treeNodeStore.rootNode;
+  selectedNode = this.treeNodeStore.selectedNode;
+
+  draggingBox = signal<ComponentRef<MyNode> | null>(null);
+  draggingBoxOffset = signal<Point>({x: 0, y: 0});
+
+  nodeComponents = signal<ComponentRef<MyNode>[]>([]);
+
+  subtree = computed(() => {
+    if (this.nodeComponents() && this.selectedNode()) {
+      const subtree: ComponentRef<MyNode>[] = [];
+      this.collectChildNodes(this.selectedNode()!, subtree);
+      return subtree;
+    }
+    return [];
+  })
+
+  viewContainer = inject(ViewContainerRef);
+  offsets: Point[] = [];
+
+  ngOnInit() {
+    let comps: ComponentRef<MyNode>[] = [];
+    this.createComponentRefList(this.rootNode() ?? {}, comps, 0);
+    this.nodeComponents.set(comps);
+  }
+
+
+
+  selectNode(node: TreeNodeDto) {
+    this.treeNodeStore.setSelectedNode(node);
+  }
+
+  startDrag(event: MouseEvent, box: ComponentRef<MyNode>) {
+    this.selectNode(box.instance.treeNode!);
+    this.draggingBox.set(box);
+    this.draggingBox()!.location.nativeElement.style.zIndex = '1000';
+    this.draggingBox()!.location.nativeElement.classList.add('no-select');
+    const draggingBoxBounds = this.draggingBox()?.location.nativeElement.getBoundingClientRect();
+    this.draggingBoxOffset.set({
+      x: event.clientX - draggingBoxBounds.x,
+      y: event.clientY - draggingBoxBounds.y
+    });
+    this.offsets = [];
+    this.subtree().forEach(node => {
+      node.location.nativeElement.style.zIndex = '1000';
+      const nodeBounds = node.location.nativeElement.getBoundingClientRect();
+      this.offsets.push({
+        x: event.clientX - nodeBounds.x,
+        y: event.clientY - nodeBounds.y
+      })
+    });
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    event.preventDefault();
+    if (!this.draggingBox()) return;
+    this.setComponentPosition(this.draggingBox()!, {
+      x: event.clientX - this.draggingBoxOffset().x,
+      y: event.clientY - this.draggingBoxOffset().y
+    });
+    this.subtree().forEach((node, index) => {
+      this.setComponentPosition(node, {
+        x: event.clientX - this.offsets[index].x,
+        y: event.clientY - this.offsets[index].y
+      });
+    });
+    this.detectOverlaps();
+  }
+
+  @HostListener('document:mouseup')
+  onMouseUp() {
+    if (this.draggingBox()) {
+      this.draggingBox()!.location.nativeElement.style.zIndex = '1';
+      this.draggingBox()!.location.nativeElement.classList.remove('no-select');
+      this.subtree().forEach(node => node.location.nativeElement.style.zIndex = '1');
+    }
+    this.draggingBox.set(null);
+    this.nodeComponents().forEach(b => b.instance.isOverlapped = false);
+  }
+
+  private detectOverlaps() {
+    if (!this.draggingBox()) return;
+
+    const draggingBoxBounds = this.draggingBox()?.location.nativeElement.getBoundingClientRect();
+
+    this.nodeComponents().forEach(compRef => {
+      const b = compRef.instance;
+      if (b === this.draggingBox()!.instance) {
+        b.isOverlapped = false;
+        return;
+      }
+
+      const componentBounds = compRef.location.nativeElement.getBoundingClientRect();
+
+      b.isOverlapped = !(
+        draggingBoxBounds.x + draggingBoxBounds.width < componentBounds.x ||
+        draggingBoxBounds.x > componentBounds.x + componentBounds.width ||
+        draggingBoxBounds.y + draggingBoxBounds.height < componentBounds.y ||
+        draggingBoxBounds.y > componentBounds.y + componentBounds.height
+      );
+    });
+  }
+
+  // private setPositions(node: TreeNodeDto, level: number) {
+  //   const compRef = this.findComponentRef(node)!;
+  //   this.setComponentPosition(compRef, {
+  //     x: level * 50,
+  //     y: compRef.length * 100
+  //   });
+  // }
+
+  private createComponentRefList(node: TreeNodeDto, comps: ComponentRef<MyNode>[], level: number) {
+    const comp = this.viewContainer.createComponent(MyNode);
+    comp.instance.isOverlapped = false;
+    comp.instance.treeNode = node;
+    comp.instance.clicked.subscribe(e => this.startDrag(e, comp));
+    this.setComponentPosition(comp, {
+      x: level * 50,
+      y: comps.length * 100
+    });
+    comps.push(comp);
+    node.children?.forEach(child => this.createComponentRefList(child, comps, level + 1));
+  }
+
+  private setComponentPosition(compRef: ComponentRef<MyNode>, position: Point) {
+    compRef.location.nativeElement.style.left = `${position.x}px`;
+    compRef.location.nativeElement.style.top = `${position.y}px`;
+  }
+
+  private collectChildNodes(node: TreeNodeDto, subtree: ComponentRef<MyNode>[]) {
+    node.children?.forEach(child => {
+      subtree.push(this.findComponentRef(child)!);
+      this.collectChildNodes(child, subtree)
+    });
+  }
+
+  private findComponentRef(node: TreeNodeDto): ComponentRef<MyNode> | undefined {
+    return this.nodeComponents().find(compRef => compRef.instance.treeNode === node);
+  }
+}
